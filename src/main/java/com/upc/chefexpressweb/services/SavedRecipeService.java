fc53fa76c3ace2622ea5902bc36f76b245548935
Katherine Carrizales <kathecharlie@hotmail.com>
2024-05-04 11:57:17 -0500

    saved_recipes

--- src/main/java/com/upc/chefexpressweb/services/SavedRecipeService.java
+++ src/main/java/com/upc/chefexpressweb/services/SavedRecipeService.java
@@ -1,12 +1,17 @@
 package com.upc.chefexpressweb.services;
 
+import com.upc.chefexpressweb.entities.SavedRecipe;
 import com.upc.chefexpressweb.repositories.SavedRecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SavedRecipeService {
     @Autowired
     private SavedRecipeRepository savedRecipeRepository;
-
+    public List<SavedRecipe> getSavedRecipeByUserId(Long userId) {
+        return savedRecipeRepository.findByUserId(userId);
+    }
 }
