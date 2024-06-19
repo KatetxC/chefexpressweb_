73a1ad1f2345af55de1c06587a776f268553c038
zzzgrucho <89281221+zzzgrucho@users.noreply.github.com>
2024-06-18 20:09:19 -0500

    Delete src directory

--- src/main/java/com/upc/chefexpressweb/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.upc.chefexpressweb.repositories;
-
-import com.upc.chefexpressweb.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
