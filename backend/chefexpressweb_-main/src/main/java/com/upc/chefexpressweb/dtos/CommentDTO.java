fff83089b73848758e71938c8c4d669f7f0f39d5
Oscar Naveda <89281221+zzzgrucho@users.noreply.github.com>
2024-06-24 07:14:52 -0500

    Add files via upload
    Final BackEnd

---
+++ backend/chefexpressweb_-main/src/main/java/com/upc/chefexpressweb/dtos/CommentDTO.java
@@ -0,0 +1,50 @@
+package com.upc.chefexpressweb.dtos;
+
+
+import com.upc.chefexpressweb.entities.Recipe;
+import com.upc.chefexpressweb.entities.Users;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+    private int idComment;
+    private Recipe recipe;
+    private int calification;
+    private String text;
+
+    public int getIdComment() {
+        return idComment;
+    }
+
+    public void setIdComment(int idComment) {
+        this.idComment = idComment;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public int getCalification() {
+        return calification;
+    }
+
+    public void setCalification(int calification) {
+        this.calification = calification;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
