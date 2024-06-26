c967a5d0294444ef6b12e28707615211a7474f3e
Oscar Naveda <89281221+zzzgrucho@users.noreply.github.com>
2024-06-26 13:27:08 -0500

    Delete backend/chefexpressweb_-main directory

--- backend/chefexpressweb_-main/src/main/java/com/upc/chefexpressweb/repositories/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upc.chefexpressweb.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.upc.chefexpressweb.entities.Role;
-
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long> {
-}
